d271e52cc10b999811096df7493ac99d0bec6caf
Eniola Akinlonu <akinlonueniola@gmail.com>
2026-03-24 18:36:07 +0100

    remove CheckTokenActivity class from the bootstrap.php file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,19 +19,12 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->api(append: [
-        //     \App\Http\Middleware\CheckTokenActivity::class,
-        // ]);
-
         $middleware->alias([
 
             // Spatie Permission middlewares:
             'role'                => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission'          => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission'  => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-
-            // Custom Middlewares
-            'check.token.activity' => \App\Http\Middleware\CheckTokenActivity::class,
             
         ]);
     })
